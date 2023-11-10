192b8dd4c1778d94f293c998ce7dc0915a56ee17
B <128028346+Barvand@users.noreply.github.com>
2023-11-10 10:07:42 +0100

    updated some responsiveness

--- js/functions/index.js
+++ js/functions/index.js
@@ -1,23 +1,36 @@
 const gridContainer = document.querySelector(".grid-item8-container");
+const errorContainer = document.querySelector(".flex-wrapper-deluxe")
 
-
+const url = "https://api.noroff.dev/api/v1/gamehub"; 
 /// functions index page
 
-export function renderHTML(results) {
+export async function renderHTML(results) {
+  try { 
+    const response = await fetch(url);
+    if (!response.ok) { 
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
   gridContainer.innerHTML = "";
 
   for (let i = 0; i < 5; i++) {
-    gridContainer.innerHTML += `<div class="grid-item item-8"> 
+    gridContainer.innerHTML += `<div class="grid-item item-8" a href="product-page.html?id=${results[i].id}"> 
                                            <a href="product-page.html?id=${results[i].id}">
                                            <div class="price-grid-item8">
                                            <h4> ${results[i].title} </h4>
                                             <p class="game-price"> $${results[i].price} </p>
-                                            <img src="${results[i].image}" alt="${results[i].title}" class="item8-image"> </a></div>`;
+                                            <img src="${results[i].image}" alt="${results[i].title}" class="item8-image"> </a></div> </div>`;
   }
+} catch (error){ 
+console.error('Error in displayContent:', error);
+errorContainer.innerHTML = `<div class="error-message"> Oops!! Something went wrong and it is our fault </div>`;
 }
+}; 
 
 export function randomImage(results) {
   const imageContainer = document.querySelector(".griditem-1");
   const randomIndex = Math.floor(Math.random() * results.length);
   imageContainer.style.backgroundImage = `url(${results[randomIndex].image})`;
 }
+
+
+
